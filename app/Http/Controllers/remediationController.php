cfdd63108084b181375ac0f1bbfc044313bab64f
Londot <bryan.londot@hotmail.com>
2020-05-14 17:05:41 +0200

    Retourner la view avec controller

---
+++ app/Http/Controllers/remediationController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class remediationController extends Controller
+{
+    public  function  index(){
+	return  view('molengeek');
+}
+}
